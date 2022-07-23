e28c60ca3d8960ae24d8d6d2d68e14d508ffc82e
Ivy <28644397+ivynganga@users.noreply.github.com>
2022-07-23 19:27:58 +0300

    updates

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Crypto Price in KES</title>
+	<title>Crypto Price in KESs</title>
 
 	<link href="/css/app.css" rel="stylesheet">
 	<link rel="stylesheet" href="/css/style.css">
