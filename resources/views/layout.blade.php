d291ef6abab938fca2e42afa2eebf3b54aa06358
Ivy <28644397+ivynganga@users.noreply.github.com>
2022-07-23 18:24:36 +0300

    css fix

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,11 +6,11 @@
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Crypto Price in KES</title>
 
-	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
-	<link rel="stylesheet" href="{{ asset('/css/style.css') }}">
+	<link href="/css/app.css" rel="stylesheet">
+	<link rel="stylesheet" href="/css/style.css">
 	
-	<script src="{{ asset('js/app.js') }}" defer></script>
-	<script src="{{ asset('js/style.js') }}" defer></script>
+	<script src="/js/app.js" defer></script>
+	<script src="js/style.js" defer></script>
 </head>
 
 
